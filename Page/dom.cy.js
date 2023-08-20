7758209a3bbc7b2e5e40ae49831186ab7caf85d9
Olatunde093 <skm.ajide@gmail.com>
2023-08-20 15:23:24 +0100

    Test to validate transactions

---
+++ Page/dom.cy.js
@@ -0,0 +1,57 @@
+class test{
+    visit(){
+
+    return cy.visit("https://parabank.parasoft.com/")
+    }
+    username(){
+       return cy.get("[name='username']")
+    }
+    password(){
+       return cy.get("[name='password']")
+    }
+    button(){
+      return cy.get("[value]")
+    }
+    AccountBalance(){
+     return cy.get("tbody tr:nth-of-type(2) .ng-binding")
+    }
+    payeeName(){
+       return cy.get("input[name='payee.name']")
+    }
+    payeeAddress(){
+       return cy.get("input[name='payee.address.street']")
+    }
+    payeeCity(){
+       return cy.get("input[name='payee.address.city']")
+    }
+    state(){
+        return cy.get("tr:nth-of-type(4) > td:nth-of-type(2)")
+    }
+    zipcode(){
+        return cy.get("input[name='payee.address.zipCode']")
+    }
+    phone(){
+        return cy.get("input[name='payee.phoneNumber']")
+    }
+    account(){
+        return cy.get("input[name='payee.accountNumber']")
+    }
+    verifyaccount(){
+        return cy.get("input[name='verifyAccount']")
+    }
+    amount(){
+        return cy.get("input[name='amount']")
+    }
+    myaccount(){
+        return cy.get("select[name='fromAccountId']")
+    }
+    sendButton(){
+        return cy.get("[value='Send Payment']")
+    }
+    transfer(){
+      return  cy.get("div#leftPanel > ul > li:nth-of-type(4) > a")
+    }
+
+    
+}
+module.exports = test;
---
+++ cypress/e2e/Test_automation/test.cy.js
@@ -0,0 +1,36 @@
+import test from '../../../Page/dom.cy'
+const tests = new test;
+
+describe("Testing automation",()=>{
+    const AccountBalance = 1800
+    const transaction = 100
+
+    beforeEach("visit the page",()=>{
+        tests.visit();
+    })
+    it("should be able get remaining balance",()=>{
+        tests.username().type("Muiz")
+        tests.password().type("faceronke")
+        tests.button().click()
+        tests.transfer().click()
+        tests.payeeName().type("muiz")
+        tests.payeeAddress().type("4 deleawelewa")
+        tests.payeeCity().type("lagos")
+        tests.state().type("Lagos")
+        tests.zipcode().type("0109200")
+        tests.phone().type("020292929")
+        tests.account().type("12334")
+        tests.verifyaccount().type("12334")
+        tests.amount().type("100")
+        tests.myaccount().select(0)
+        tests.sendButton().click()
+        cy.get("div#leftPanel > ul > li:nth-of-type(2) > a").click()
+
+        const Balance = AccountBalance - transaction;
+        
+        tests.AccountBalance().should('contain',Balance)
+
+
+
+    })
+})
